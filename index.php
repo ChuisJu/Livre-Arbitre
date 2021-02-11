89560dbde075695b4e6dec2774b91dd79d6b6602
Methyl <73468685+ScriptingForLife@users.noreply.github.com>
2021-02-11 15:27:45 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- css -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="./css/index.css">
+        <!-- script -->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+        <title></title>
+    </head>
+    <body>
+
+        <!-- NAVBAR -->
+        <nav class="navbar navbar-expand-lg navbar navbar-dark bg-primary">
+            <a href="./index.php" class="navbar-brand">Livre Arbitre</a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse2">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse2">
+                <div class="navbar-nav">
+                    
+                    <a href="./index.php" class="nav-item nav-link active">Accueil</a>
+                    <a href="./livres.php" class="nav-item nav-link">Livres</a>
+                    <a href="./contact.php" class="nav-item nav-link">Contact</a>
+                </div>
+                <form class="form-inline ml-auto">
+                <div class="navbar-nav">
+                    <a href="./login.php" class="nav-item nav-link">Espace client</a>
+                </div>
+                </form>
+            </div>
+        </nav>
+
+        <!-- MAIN CONTENT -->
+        
+        <div class="main">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="panel">
+                            lol
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="panel">
+                            lol
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="panel">
+                            lol
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- FOOTER -->
+        <div class="fixed footer-basic">
+            <footer>
+                <div class="social">
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-snapchat"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-google"></i></a>
+                </div>
+                <ul class="list-inline">
+                    <li class="list-inline-item"><a href="#">Accueil</a></li>
+                    <li class="list-inline-item"><a href="#">Livres</a></li>
+                    <li class="list-inline-item"><a href="#">Contact</a></li>
+                    <li class="list-inline-item"><a href="#">Espace client</a></li>
+                </ul>
+                <p class="copyright">Livre Arbitre © 2021</p>
+            </footer>
+        </div>
+    </div>
+    </body>
+</html>
