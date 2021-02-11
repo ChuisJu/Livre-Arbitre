678b5db288f779892beb075fae9d57f323cba221
MDKayzeur <74770566+MDKayzeur@users.noreply.github.com>
2021-02-11 17:50:32 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -4,15 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <!-- CSS -->
+        <!-- css -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="./css/header.css">
-        <link rel="stylesheet" href="./css/footer.css">
         <link rel="stylesheet" href="./css/index.css">
-
-        <!-- SCRIPT -->
-
+        <!-- script -->
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -29,18 +25,30 @@
             <div class="container">
                 <div class="row">
                     <div class="col">
-                        <div class="panel">
-                            lol
+                        <div class="panel1">
+                            <?php 
+                            [I M A G E]<br>
+                            Titre : <br>
+                            Auteur : <br>
+                            ?> 
                         </div>
                     </div>
                     <div class="col">
-                        <div class="panel">
-                            lol
+                        <div class="panel2">
+                            <?php 
+                            [I M A G E]<br>
+                            Titre : <br>
+                            Auteur : <br>
+                            ?> 
                         </div>
                     </div>
                     <div class="col">
-                        <div class="panel">
-                            lol
+                        <div class="panel3">
+                            <?php 
+                            [I M A G E]<br>
+                            Titre : <br>
+                            Auteur : <br>
+                            ?> 
                         </div>
                     </div>
                 </div>
