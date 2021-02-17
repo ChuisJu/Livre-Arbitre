7d6d003d409e49b1ecf9f134b6818421b600534c
Methyl <73468685+ScriptingForLife@users.noreply.github.com>
2021-02-17 17:18:33 +0100

    Add files via upload

--- inc/footer.php
+++ inc/footer.php
@@ -1,1 +1,20 @@
+<!-- FOOTER -->
 
+<div class="fixed footer-basic">
+    <footer>
+        <div class="social">
+            <a href="#"><i class="fa fa-instagram"></i></a>
+            <a href="#"><i class="fa fa-snapchat"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-google"></i></a>
+        </div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="#">Accueil</a></li>
+            <li class="list-inline-item"><a href="#">Livres</a></li>
+            <li class="list-inline-item"><a href="#">Contact</a></li>
+            <li class="list-inline-item"><a href="#">Espace client</a></li>
+        </ul>
+        <p class="copyright">Livre Arbitre © 2021</p>
+    </footer>
+</div>
