d13a2ce293fcbaa25b39d69a4f1a2618dd8bd36f
Antoine Benoit <antoine.benoit@epita.fr>
2021-02-18 16:28:22 +0100

    compte utilisateur connexion

--- login.php
+++ login.php
@@ -9,8 +9,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="./css/header.css">
         <link rel="stylesheet" href="./css/footer.css">
-        <link rel="stylesheet" href="./css/login.css">
-        <link rel="stylesheet" href="./css/gridn.css">
+        <link rel="stylesheet" href="./css/index.css">
 
         <!-- SCRIPT -->
 
@@ -22,81 +21,42 @@
     </head>
     <body>
 
-        <?php
+        <?php 
         include 'inc/header.php';
+        include 'inc/db_conn.php';
         ?>
 
         <!-- MAIN CONTENT -->
 
-        <div class="main">
-            <div class="container"><br><br><br>
-				<div class ="grille">
-		<div class="formulaire"><form method="post" action="inscription.php">
-            <fieldset class="formulaire">
-                <fieldset class="formulaire">
-                <legend>Vos informations de compte</legend>
-                
-                    <label for="name">Nom :</label><br>
-                    <input type="text" id="name" name="name" autofocus required>
-                    <br>
-                    <label for="first_name">Prénom :</label><br>
-                    <input type="text" id="first_name" name="first_name" ><br>
-                    <label for="name">Identifiant de la carte bibliothèque :</label><br>
-                    <input type="text" id="id" name="id" autofocus required>
-                    <br>
-                    <label for="name">Mot de passe :</label><br>
-                    <input type="password" id="password" name="password" required>
-                    <br>
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $mdp=$_POST["password"];
+        //$mdp=password_hash(($_POST["password"]), PASSWORD_DEFAULT);
+/*        $username = $_POST["user_name"];
+        echo"$mdp $username";
+        $user = $link->query("SELECT mdp FROM utilisateur WHERE utilisateur=$username")->fetch_assoc();
+        if($mdp==$username["mdp"])
+        {*/
+			echo "<table align='center'><tr><th>Livres empruntés</th></tr><tr><td>Titre</td><td>isbn</td><td>Date d'emprunt</td></tr>";
+			$livres = $link->query("SELECT titre, livre.isbn AS livreid,dateEmpreint FROM utilisateur JOIN emprunt ON utilisateur.idUtilisateur=emprunt.idUtilisateur JOIN livre ON emprunt.isbn=livre.isbn WHERE utilisateur=$user_name");
+			 while($row = mysqli_fetch_array($livres, MYSQLI_ASSOC))
+			{
+				echo "<tr>";
+				echo "oui";
+				echo "<td>".$row["titre"]."</td><td>".$row["livreid"]."</td>".$row["dateEmpreint"]."</td>";
+				echo "</tr>";
+			}
+			echo "</table>";
+			
+		 /*}
+		 else{echo"<b>Mot de passe incorect!</b>";}*/
 
-                    
-                    
-                    
-
-                    <label for="name">Mail :</label><br>
-                    <input type="email" id="mail" name="mail" required><br>
-					
-					<label>Date de Naissance</label><br>
-					<label for="anneeNaiss">Année :</label>
-					<select name="anneeNaiss" required><br>
-					
-                  <?php
-                  for($i=2020;$i>1920;$i--)
-                  {
-					echo"<option value='".$i."'>".$i."</option>";
-                  }
-                  ?>
-                  </select>
-                  <label for="moisNaiss">Mois :</label>
-					<select name="moisNaiss" required><br>
-					<?php
-                  for($i=1;$i<13;$i++)
-                  {
-					echo"<option value='".$i."'>".$i."</option>";
-                  }
-                  ?>
-					</select>
-					<label for="jourNaiss">Jour :</label>
-					<select name="jourNaiss" required><br>
-					<?php
-                  for($i=1;$i<32;$i++)
-                  {
-					echo"<option value='".$i."'>".$i."</option>";
-                  }
-                  ?>
-					</select>
-                  
-
-
-               
-            </fieldset>
-            <br>
-            <div align="center">
-            <button class="formulaire" type="submit">Confirmer</button></fieldset>
-            </div>
-        </form></div></div>
-        <br><br><br>
-            </div>
-        </div>
+	}
+        
+        
+		
+?>
 
         <?php include 'inc/footer.php'; ?>
     </body>
