0c2cfee367ba9c58d84e51cfe5d74d5ed6afc65b
Methyl <73468685+ScriptingForLife@users.noreply.github.com>
2021-02-18 18:04:13 +0100

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -13,7 +13,6 @@
         </div>
         <ul class="list-inline">
             <li class="list-inline-item"><a href='<?php echo $CONFIG['root_path'] . "index.php" ?>'>Accueil</a></li>
-            <li class="list-inline-item"><a href='<?php echo $CONFIG['root_path'] . "livres.php" ?>'>Livres</a></li>
             <li class="list-inline-item"><a href='<?php echo $CONFIG['root_path'] . "contact.php" ?>'>Contact</a></li>
             <li class="list-inline-item"><a href='<a href='<?php echo $CONFIG['root_path'] . "index.php" ?>'>Espace client</a></li>
         </ul>
