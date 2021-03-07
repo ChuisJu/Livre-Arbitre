826d3fd77a48a5d5ca12d1aae750ce90bf9b6e43
Antoine Benoit <antoine.benoit@epita.fr>
2021-03-07 21:12:31 +0100

    Petite modification de l'espace client

--- espace/index.php
+++ espace/index.php
@@ -46,7 +46,7 @@
         <a> Votre numéro de carte est : <?php echo"$numcarte" ?> </a></br>
         <a href="../disconnect.php"> se déconnecter </a><br>
         <a href="emprunt.php">voir les livres empruntés</a><br>
-        <a href="avis.php">Voir les livres likés et les commentaires</a>
+        <!--<a href="avis.php">Voir les livres likés et les commentaires</a>-->
       </div>
       <?php include '../inc/footer.php'; ?>
     </body>
