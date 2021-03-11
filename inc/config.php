b53a6416f27f10611f2119398170460018a96233
Julien Dubois <74368850+ChuisJu@users.noreply.github.com>
2021-03-11 17:30:16 +0100

    Add files via upload

--- inc/config.php
+++ inc/config.php
@@ -1,11 +1,11 @@
 <?php
 
-$CONFIG = array('root_path' => 'http://' . $_SERVER['SERVER_NAME'] . "/exercice/Livre-Arbitre-main/",
-                'dbhost' => 'localhost',
-                'dbuser' => 'Biblioreader',
-                'dbpwd' => 'SuperRoot77',
+$CONFIG = array('root_path' => '/',
+                'dbhost' => 'ipssisqlivrearbi.mysql.db',
+                'dbuser' => 'ipssisqlivrearbi',
+                'dbpwd' => 'Gnr9Yp8QdC268',
                 'dbport' => '3306',
-                'dbname' => 'livrearbitre'
+                'dbname' => 'ipssisqlivrearbi'
             );
 
 ?>
