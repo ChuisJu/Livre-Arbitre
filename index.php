822b3bfcd027f84df4d02264a1e52b99377d4764
Julien Dubois <74368850+ChuisJu@users.noreply.github.com>
2023-04-04 15:02:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-        <title></title>
+        <title>GROS TITRE MAGNIFIQUE</title>
     </head>
     <body>
 
